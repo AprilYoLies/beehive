9bed29b2699fbdc1a1e18a9e2274c2c1f7c31e8f
AprilYoLies <863821569@qq.com>
2019-06-20 15:33:32 +0800

    添加了对于心跳事件的发送，用来验证客户端和服务端逻辑，接下来对功能进行验证

--- beehive-core/src/main/java/top/aprilyolies/beehive/transporter/server/handler/ClientFinalChannelHandler.java
+++ beehive-core/src/main/java/top/aprilyolies/beehive/transporter/server/handler/ClientFinalChannelHandler.java
@@ -1,6 +1,11 @@
 package top.aprilyolies.beehive.transporter.server.handler;
 
 import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.timeout.IdleStateEvent;
+import org.apache.log4j.Logger;
+import top.aprilyolies.beehive.transporter.server.message.MessageType;
+import top.aprilyolies.beehive.transporter.server.message.Request;
 
 /**
  * @Author EvaJohnson
@@ -8,4 +13,20 @@ import io.netty.channel.ChannelDuplexHandler;
  * @Email g863821569@gmail.com
  */
 public class ClientFinalChannelHandler extends ChannelDuplexHandler {
+    private static final Logger logger = Logger.getLogger(ClientFinalChannelHandler.class);
+
+    @Override   // 心跳消息的传播
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent) {
+            // handler: NettyClient -> MultiMessageHandler -> HeartBeatHandler -> AllChannelHandler -> （AllChannelHandler 持有）DecodeHandler -> HeaderExchangeHandler -> DubboProtocol$1
+            if (logger.isDebugEnabled()) {
+                logger.debug("IdleStateEvent triggered, send heartbeat to server");
+            }
+            Request req = new Request();
+            req.setType(MessageType.HEARTBEAT_REQUEST);
+            ctx.writeAndFlush(req);
+        } else {
+            super.userEventTriggered(ctx, evt);
+        }
+    }
 }
