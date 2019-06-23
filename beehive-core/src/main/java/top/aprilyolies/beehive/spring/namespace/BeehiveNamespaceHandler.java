908ad729c4bf7bdc00b1b64257436ac0700c2e43
AprilYoLies <863821569@qq.com>
2019-06-23 11:36:06 +0800

    搭建好 beehive namespace 解析的基本框架

---
+++ beehive-core/src/main/java/top/aprilyolies/beehive/spring/namespace/BeehiveNamespaceHandler.java
@@ -0,0 +1,15 @@
+package top.aprilyolies.beehive.spring.namespace;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+/**
+ * @Author EvaJohnson
+ * @Date 2019-06-23
+ * @Email g863821569@gmail.com
+ */
+public class BeehiveNamespaceHandler extends NamespaceHandlerSupport {
+    @Override
+    public void init() {
+        registerBeanDefinitionParser("registry", new RegistryBeanDefinitionParser());
+    }
+}
---
+++ beehive-core/src/main/java/top/aprilyolies/beehive/spring/namespace/ReferenceBeanDefinitionParser.java
@@ -0,0 +1,18 @@
+package top.aprilyolies.beehive.spring.namespace;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.xml.BeanDefinitionParser;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.w3c.dom.Element;
+
+/**
+ * @Author EvaJohnson
+ * @Date 2019-06-23
+ * @Email g863821569@gmail.com
+ */
+public class ReferenceBeanDefinitionParser implements BeanDefinitionParser {
+    @Override
+    public BeanDefinition parse(Element element, ParserContext parserContext) {
+        return null;
+    }
+}
