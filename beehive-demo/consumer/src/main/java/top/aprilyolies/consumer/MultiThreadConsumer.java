d24f0bc5e51cfadfb3bd1341a82428ae18230527
AprilYoLies <863821569@qq.com>
2019-06-27 15:13:39 +0800

    重新设计多线程访问 consumer 函数

--- beehive-demo/consumer/src/main/java/top/aprilyolies/consumer/MultiThreadConsumer.java
+++ beehive-demo/consumer/src/main/java/top/aprilyolies/consumer/MultiThreadConsumer.java
@@ -3,42 +3,60 @@ package top.aprilyolies.consumer;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import top.aprilyolies.service.BeehiveService;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * @Author EvaJohnson
  * @Date 2019-06-23
  * @Email g863821569@gmail.com
  */
 public class MultiThreadConsumer {
+    // 拟测试的线程数
+    private static int THREADS = 10;
+
     public static void main(String[] args) throws Exception {
+        // 计数栅栏
+        CountDownLatch latch = new CountDownLatch(THREADS);
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("consumer.xml");
         context.start();
         BeehiveService demoService = context.getBean("demoService", BeehiveService.class);
-//        ExecutorService executor = Executors.newCachedThreadPool(new ThreadFactory() {
-//            int id = 0;
-//
-//            @Override
-//            public Thread newThread(Runnable r) {
-//                Thread thread = new Thread(r);
-//                thread.setName("MultiThreadConsumer-pool-thread-" + id++);
-//                return thread;
-//            }
-//        });
-//        for (int i = 0; i < 10; i++) {
-//            executor.submit(() -> {
-//                try {
-//                    for (int i1 = 0; i1 < 1000; i1++) {
-//                        BeehiveService service = context.getBean("demoService", BeehiveService.class);
-//                        String hello = service.say(Thread.currentThread().getName() + " - " + i1);
-//                        System.out.println("result: " + hello);
-//                    }
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            });
+        ExecutorService executor = Executors.newCachedThreadPool(new DemoThreadFactory());
+        // 多个线程进行访问
+        for (int i = 0; i < THREADS; i++) {
+            executor.submit(() -> {
+                try {
+                    for (int i1 = 0; i1 < 1000; i1++) {
+                        BeehiveService service = context.getBean("demoService", BeehiveService.class);
+                        String hello = service.say(Thread.currentThread().getName() + " - " + i1);
+                        System.out.println("result: " + hello);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+//        for (int i = 0; i < 2000; i++) {
+//            String hello = demoService.say("world - " + i);
+//            System.out.println("result: " + hello);
 //        }
-        for (int i = 0; i < 1000; i++) {
-            String hello = demoService.say("world - " + i);
-            System.out.println("result: " + hello);
+        latch.await();
+    }
+
+    private static class DemoThreadFactory implements ThreadFactory {
+        AtomicLong id = new AtomicLong(1);
+
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread thread = new Thread(r);
+            thread.setName("MultiThreadConsumer-pool-thread-" + id.getAndIncrement());
+            thread.setDaemon(true);
+            return thread;
         }
     }
 }
